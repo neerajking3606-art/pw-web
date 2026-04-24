2848af901ab9dc87cf4269d7441d4b9adb138a01
neerajking3606-art <neerajking3606@gmail.com>
2026-04-24 15:53:19 +0530

    Delete api directory

--- api/webhook.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const { message } = req.body;
-  if (message?.text) {
-    console.log('New token:', message.text);
-    // Vercel env update नहीं, file में save या alert
-    res.json({ ok: true });
-  }
-  res.status(200).end('OK');
-}
